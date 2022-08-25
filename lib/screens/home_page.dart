c15a9dba3c5c4168d9ccf4b1a05b3449fc96496f
burakgurler16 <burakgurlerweb@gmail.com>
2022-08-25 16:01:35 +0300

    current week card update

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intl/intl.dart';
+import 'package:week_of_year/week_of_year.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -21,31 +23,54 @@ class _HomePageState extends State<HomePage> {
         margin: const EdgeInsets.all(10.0),
         child: Column(
           children: [
-            Expanded(
-              flex: 3,
-              child: Row(
-                children: [
-                  _InfoCard(),
-                  _InfoCard(),
-                ],
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [Expanded(child: _InfoCard()), Expanded(child: _InfoCard())],
             ),
-            Expanded(
-                flex: 7,
-                child: ListView(
-                  children: const [
-                    _DaysCard(day: 'pazartesi'),
-                    _DaysCard(day: 'salı'),
-                    _DaysCard(day: 'çarşamba'),
-                    _DaysCard(day: 'perşembe'),
-                    _DaysCard(day: 'cuma'),
-                    _DaysCard(day: 'cumartesi'),
-                    _DaysCard(day: 'pazar'),
-                  ],
-                ))
+            Divider(),
+            _CurrentWeekCard(context),
+            Divider(),
+            Expanded(child: _dayList())
           ],
         ),
       ),
+    );
+  }
+
+  Card _CurrentWeekCard(BuildContext context) {
+    return Card(
+      color: Colors.amber[200],
+      child: Container(
+        child: Text(
+          thisWeek(),
+          style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.black),
+        ),
+        margin: const EdgeInsets.all(5.0),
+        width: MediaQuery.of(context).size.width,
+        height: 20.0,
+        alignment: Alignment.center,
+      ),
+    );
+  }
+
+  String thisWeek() {
+    final now = DateTime.now();
+    final dayOfWeek = DateFormat('E');
+    String day = dayOfWeek.format(now);
+    return ('${now.weekOfYear}. Hafta\n$day');
+  }
+
+  ListView _dayList() {
+    return ListView(
+      children: const [
+        _DaysCard(day: 'pazartesi'),
+        _DaysCard(day: 'salı'),
+        _DaysCard(day: 'çarşamba'),
+        _DaysCard(day: 'perşembe'),
+        _DaysCard(day: 'cuma'),
+        _DaysCard(day: 'cumartesi'),
+        _DaysCard(day: 'pazar'),
+      ],
     );
   }
 }
@@ -60,7 +85,7 @@ class _InfoCard extends StatelessWidget {
     return Card(
       color: Colors.amber,
       child: SizedBox(
-        width: MediaQuery.of(context).size.width / 3,
+        width: MediaQuery.of(context).size.width / 2,
         height: 150,
         child: Center(
           child: Column(
