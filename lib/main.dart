b6146f3ce0ba22cf48cfb2c25db273634057bac5
burakgurler16 <burakgurlerweb@gmail.com>
2022-08-30 14:59:00 +0300

    weather info oluşturuldu

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:weather/model/weather_model.dart';
-import 'package:weather/screens/home_page.dart';
 import 'package:weather/screens/loading_page.dart';
+import 'package:weather/utils/weather_data.dart';
 
 void main() {
   runApp(const MyApp());
@@ -18,70 +14,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData.dark(),
-      home: HomePage(),
-
-      // home: const MyHomePage(title: 'Api deneme Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  String? _gelenCevap;
-  double? latitude = 40.2763;
-  double? longitude = 29.1035;
-  final String _apiKey = 'a444db179d1c4f7f3b4169f8cc39f837';
-
-  Info? _dayInfo;
-
-  Future<void> _incrementCounter() async {
-    var adres = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=${_apiKey}&units=metric&lang=tr');
-    Response cevap = await get(adres);
-
-    if (cevap.statusCode == 200) {
-      Map _gelenJson = jsonDecode(cevap.body);
-      _dayInfo = Info.fromJson(_gelenJson);
-
-      _gelenCevap = _gelenJson['weather'][0]['main'];
-    } else {
-      _gelenCevap = 'bağlantı hatası';
-    }
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView(
-        children: <Widget>[
-          const Text(
-            'Gelen Cevap',
-          ),
-          Text(_dayInfo?.name ?? 'name null'),
-          Text(_dayInfo?.main?.temp.toString() ?? 'temp null'),
-          Text(
-            '$_gelenCevap',
-            style: Theme.of(context).textTheme.headline4,
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: LoadingPage(),
+      // home: WeatherData(title: 'deneme 2'),
     );
   }
 }
